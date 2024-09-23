4b2e865ed7718efef8571ddbef4f50695a48e4df
Orenge <gunkunlapat@gmail.com>
2024-09-24 02:32:15 +0700

    more content!!!

--- src/Main.elm
+++ src/Main.elm
@@ -4,6 +4,9 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 view model = 
-    h1 [] [text "haiwo :3"]
+    div [ class "main" ] [ 
+        h1 [] [text "Haiwo, this page is under construction!!"]
+        , p [] [text "Orenge is in the process of learning elm rn. idk when they will git gud tho :3c"] 
+    ]
 main = 
     view "Orenge's Page"
