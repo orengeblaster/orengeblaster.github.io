0160f9fe854d4de0ac2160846a569bd4e11c1849
Orenge <gunkunlapat@gmail.com>
2024-09-24 02:35:53 +0700

    see if the linter is correct

--- src/Main.elm
+++ src/Main.elm
@@ -3,10 +3,16 @@ module Main exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-view model = 
-    div [ class "main" ] [ 
-        h1 [] [text "Haiwo, this page is under construction!!"]
-        , p [] [text "Orenge is in the process of learning elm rn. idk when they will git gud tho :3c"] 
-    ]
-main = 
+
+view : a -> Html msg
+view _ =
+    div [ class "main" ]
+        [ h1 [] [ text "Haiwo, this page is under construction!!" ]
+        , p []
+            [ text "Orenge is in the process of learning elm rn. idk when they will git gud tho :3c" ]
+        ]
+
+
+main : Html msg
+main =
     view "Orenge's Page"
