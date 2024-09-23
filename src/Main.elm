d830f3dbc7e605499cf7ef1e8fa19e8f4cf26ca1
Orenge <gunkunlapat@gmail.com>
2024-09-24 04:17:46 +0700

    optimise length

--- src/Main.elm
+++ src/Main.elm
@@ -3,16 +3,10 @@ module Main exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
-
-view : a -> Html msg
-view _ =
+main : Html msg
+main =
     div [ class "main" ]
         [ h1 [] [ text "Haiwo, this page is under construction!!" ]
         , p []
             [ text "Orenge is in the process of learning elm rn. idk when they will git gud tho :3c" ]
         ]
-
-
-main : Html msg
-main =
-    view "Orenge's Page"
